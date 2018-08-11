1cfd399e1d603ed17df5615c483564e85edcfcc6
curiousclarissa <clarissa.jarem@gmail.com>
2018-08-10 18:54:39 -0700

    word adapter now allows colors to be set by category

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -1,6 +1,7 @@
 package com.example.android.miwok;
 
 import android.content.Context;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,12 +12,19 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 /**
- * Created by clarissajarem on 8/7/18.
+  Created by clarissajarem on 8/7/18.
+  @param context is the current context (i.e. Activity) that the adapter is being created in.
+     * @param words is the list of {@link Word}s to be displayed.
+     * @param colorResourceId is the resource ID for the background color for this list of words
  */
+/* Resource ID for the background color for this list of words */
+
 
 public class WordAdapter extends ArrayAdapter<Word> {
-    public WordAdapter(Context context, ArrayList<Word> words) {
+    private int mColorResourceId;
+    public WordAdapter(Context context, ArrayList<Word> words, int colorResourceId) {
         super(context,0, words );
+        mColorResourceId = colorResourceId;
     }
 
     @Override
@@ -54,6 +62,13 @@ public class WordAdapter extends ArrayAdapter<Word> {
         } else {
             iconView.setVisibility(View.GONE);
         }
+
+        // Set the theme color for the list item
+        View textContainer = listItemView.findViewById(R.id.text_container);
+        // Find the color that the resource ID maps to
+        int color = ContextCompat.getColor(getContext(), mColorResourceId);
+        // Set the background color of the text container View
+        textContainer.setBackgroundColor(color);
 
         // Return the whole list item layout (containing 2 TextViews and an ImageView)
         // so that it can be shown in the ListView
